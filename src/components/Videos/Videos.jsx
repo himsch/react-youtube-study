0749aea361db1d69ac5d7336ef4b69c3d8a6020f
song <55rsearchco@gmail.com>
2023-03-02 15:41:49 +0900

    Context 사용해 인스턴스 하나로 관리하기

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -2,19 +2,16 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../VideoCard/VideoCard';
-import FakeYoutube from '../../api/fakeYoutube';
-import Youtube from '../../api/youtube';
+import { useYoutubeApi } from '../../context/YoutubeApiContext';
 
 function Videos(props) {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(['videos', keyword], () => {
-    const youtube = new FakeYoutube();
-    return youtube.search(keyword);
-  });
+  } = useQuery(['videos', keyword], () => youtube.search(keyword));
 
   return (
     <>
---
+++ src/context/YoutubeApiContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext } from 'react';
+import Youtube from '../api/youtube';
+import FakeYoutube from '../api/fakeYoutube';
+
+const YoutubeApiContext = createContext();
+
+const youtube = new FakeYoutube();
+export function YoutubeApiProvider({ children }) {
+  return (
+    <YoutubeApiContext.Provider value={{ youtube }}>
+      {children}
+    </YoutubeApiContext.Provider>
+  );
+}
+
+export function useYoutubeApi() {
+  return useContext(YoutubeApiContext);
+}
