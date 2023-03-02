3ffe7624075ce4a855e446af34ee5d2b927db11a
song <55rsearchco@gmail.com>
2023-03-02 13:43:51 +0900

    React Query 사용해 데이터 받아오기

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import VideoCard from '../VideoCard/VideoCard';
 
 function Videos(props) {
   const { keyword } = useParams();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(['videos', keyword], async () =>
+    fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
+      .then(res => res.json())
+      .then(data => data.items)
+  );
 
-  return <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>;
+  return (
+    <>
+      <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 🙃</p>}
+      {videos && (
+        <ul>
+          {videos.map(video => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
 
 export default Videos;
