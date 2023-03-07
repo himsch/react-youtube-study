dde011f13ab54897654e4ca3c21113eed8526017
song <55rsearchco@gmail.com>
2023-03-07 11:28:08 +0900

    Youtube API와 Fake API 리팩토링, timeago이용해 로케일별 날짜표기

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { formatAgo } from '../../util/date';
 
 function VideoCard({ video }) {
-  return <div>{video.snippet.title}</div>;
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  return (
+    <li>
+      <img src={thumbnails.medium.url} alt={title} />
+      <div>
+        <p>{title}</p>
+        <p>{channelTitle}</p>
+        <p>{formatAgo(publishedAt)}</p>
+      </div>
+    </li>
+  );
 }
 
 export default VideoCard;
--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,10 +1,13 @@
 import { createContext, useContext } from 'react';
 import Youtube from '../api/youtube';
-import FakeYoutube from '../api/fakeYoutube';
+import FakeYoutubeClient from '../api/fakeYoutubeClient';
+import YoutubeClient from '../api/youtubeClient';
 
 const YoutubeApiContext = createContext();
 
-const youtube = new FakeYoutube();
+// const client = new FakeYoutubeClient();
+const client = new YoutubeClient();
+const youtube = new Youtube(client);
 export function YoutubeApiProvider({ children }) {
   return (
     <YoutubeApiContext.Provider value={{ youtube }}>
